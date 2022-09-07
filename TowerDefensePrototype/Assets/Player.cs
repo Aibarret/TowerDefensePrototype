470f583fafc35f3b1e0fca5e65e6ffa77bbe4a56
Aibarret <77796971+Aibarret@users.noreply.github.com>
2022-09-07 15:21:15 -0400

    base classes created

---
+++ TowerDefensePrototype/Assets/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int money;
+    public int killCount;
+    public int lives;
+
+    public bool loseLife()
+    {
+        return false;
+    }
+
+    public void 
+}
---
+++ TowerDefensePrototype/Assets/Tower.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public int cost = 5;
+    public float projectileSpeed = 1f;
+    public int projectileDamage = 1;
+    public int fireRate = 1;
+    public float range = 1f;
+    public GameObject Target;
+    private bool ShouldBeShooting = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
