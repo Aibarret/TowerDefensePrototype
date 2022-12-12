d838c6550579802d254f5c35c63383129cf726f8
Aibarret <77796971+Aibarret@users.noreply.github.com>
2022-12-12 12:23:36 -0500

    Emergency Push

--- TowerDefensePrototype/Assets/Inheritance & Interfaces/CharacterClasses.cs
+++ TowerDefensePrototype/Assets/Inheritance & Interfaces/CharacterClasses.cs
@@ -2,17 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterClasses : MonoBehaviour
+public abstract class Character
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject controller;
+
+    public abstract int speed { get; }
+
+    public abstract void speak();
+
+    public virtual void walk()
     {
-        
+        controller.transform.position += new Vector3(speed, 0, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public abstract void attack();
+}
+
+public class Human : Character
+{
+    public override int speed { get => 2; }
+
+    public override void speak()
     {
-        
+        Debug.Log("I am a human");
     }
+
+    public override void attack()
+    {
+        Debug.Log("The Human swings a sword!");
+    }
+
+    
+}
+
+public class Orc : Character
+{
+    public override int speed { get => 1; }
+    public override void speak()
+    {
+        Debug.Log("I am a Orc");
+    }
+
+    public override void attack()
+    {
+        Debug.Log("The Orc swings their hammer!");
+    }
+
+
+}
+
+public class Elf : Character
+{
+    public override int speed { get => 3; }
+    public override void speak()
+    {
+        Debug.Log("I am a Elf");
+    }
+    public override void attack()
+    {
+        Debug.Log("The Elf draws their bow!");
+    }
+
+
 }
